391f28006d9d52edd3d4a243b73fadb475bb6241
Joko Sarjono S <johno.smakaduta@gmail.com>
2020-11-28 20:26:20 +0700

    Hook Timer

--- App.js
+++ App.js
@@ -1,66 +1,33 @@
-import React, { useState } from 'react';
-import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import React, { useState, useEffect } from 'react';
+import { Text, View } from "react-native"
  
+// TODO (1) We make our Function Component
 const App = () => {
-    // TODO (1) Create score object state using useState
-    const [score, setScore] = useState(0) // default value is 0
+    // TODO (2) Create our states using useState, it looks like a tupple, 
+    // but it's actually an array of 0 for the state object 
+    // & 1 for the function to change the state. Lastly, there's a useState(0)
+    // it's to define our default value
+    const [seconds, setSeconds] = useState(0)
  
+    // TODO (3) Using useEffect to trigger our setInterval functions
+    useEffect(() => {
+        // TODO (4) Creating interval reference with setInterval and calls setSeconds
+        // to change our state value
+        const interval = setInterval(() => {
+            setSeconds(seconds + 1)
+        }, 1000); // in milliseconds
+        return () => clearInterval(interval)
+    }, [seconds])
+ 
+    // TODO (5) Create the View
     return (
-        <View style={styles.mainContainer}>
-            <StatusBar backgroundColor={"rgb(220,81,83)"} barStyle={"light-content"} />
-            <View style={styles.scoreContainer}>
-                <Text style={styles.scoreTitleText}>{"Score"}</Text>
-                {/* TODO (2) asign score state for our Text */}
-                <Text style={styles.scoreValueText}>{score}</Text>
-            </View>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    style={styles.buttonStyle}
-                    // TODO (3) call setScore function that we've created from useState 
-                    // inside onPress to increase the score value
-                    onPress={() => setScore(score + 1)}>
-                    <Text style={styles.buttonText}>{"+"}</Text>
-                </TouchableOpacity>
-                <View style={styles.horizontalSeparator} />
-                <TouchableOpacity
-                    style={styles.buttonStyle}
-                    // TODO (4) call setScore function that we've created from useState 
-                    // inside onPress to decrease the score value
-                    onPress={() => setScore(score - 1)}>
-                    <Text style={styles.buttonText}>{"-"}</Text>
-                </TouchableOpacity>
+        <View style={{ flex: 1, margin: 16 }}>
+            <View>
+                {/* TO DO (6) call seconds, which is the state object we've created in TODO (2) */}
+                <Text>{"Seconds : " + seconds}</Text>
             </View>
         </View>
     )
 }
- 
-const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        padding: 16,
-        backgroundColor: "rgb(240,81,83)"
-    },
-    scoreContainer: {
-        alignItems: "center"
-    },
-    scoreTitleText: {
-        textAlign: "center", fontSize: 64, color: "#FFF8DE"
-    },
-    scoreValueText: {
-        textAlign: "center", fontSize: 128, color: "#FFF8DE"
-    },
-    buttonContainer: {
-        flexDirection: "row", marginTop: 8,
-    },
-    buttonStyle: {
-        flex: 1, backgroundColor: "#FFF8DE", aspectRatio: 1.5, alignItems: "center", justifyContent: "center"
-    },
-    buttonText: {
-        fontSize: 64, color: "rgb(240,81,83)"
-    },
-    horizontalSeparator: {
-        marginHorizontal: 8
-    }
-})
  
 export default App
